f3a04bac8a01d98a61b332df094e90bfa0de72ae
djuarez2652 <djuarez0440@gmail.com>
2024-09-17 23:39:48 -0400

    style: fix string to pass test; all tests pass

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteControllerUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteControllerUnitTests.java
@@ -49,9 +49,9 @@ public class RouteControllerUnitTests {
 
   @Test
   public void indexTest() {
-    String expectedString = "Welcome, in order to make an API"
+    String expectedString = "Welcome, in order to make an API "
             + "call direct your browser or Postman to an endpoint "
-            + "\n\n This can be done using the following format: \n\n http:127.0.0"
+            + "\n\n This can be done using the following format: \n\n http://127.0.0"
             + ".1:8080/endpoint?arg=value";
     assertEquals(expectedString, mockRouteController.index());
   }
